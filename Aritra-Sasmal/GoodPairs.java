412450694a51a1091368c0ff16d65db2aa3acadc
Aritra Sasmal <aritrasasmal@gmail.com>
2022-08-27 04:02:04 +0530

    Solution:Number of Good Pairs

---
+++ Aritra-Sasmal/GoodPairs.java
@@ -0,0 +1,10 @@
+public class GoodPairs {
+    public static int numIdenticalPairs(int[] nums) {
+        int cnt = 0;
+        for(int i=0; i<nums.length-1; i++)
+            for(int j=i+1; j<nums.length; j++)
+                if(nums[i] == nums[j])
+                    cnt++;
+        return cnt;
+    }
+}
